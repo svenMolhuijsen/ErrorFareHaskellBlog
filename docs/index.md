19186b2ea8354aaf1adb50f1b5acd8bb5895d160
Sven Molhuijsen <sven.molhuijsen@ruma-vsm.nl>
2020-03-17 21:33:22 +0100

    Made Pong game in hask(h)ell

--- docs/index.md
+++ docs/index.md
@@ -1,37 +1,51 @@
-## Welcome to GitHub Pages
 
-You can use the [editor on GitHub](https://github.com/svenMolhuijsen/ErrorFareHaskellBlog/edit/master/index.md) to maintain and preview the content for your website in Markdown files.
+##Haskell Error fare web scraper development blog
 
-Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
+This blog is created to give insights in my attempt to create a web scraper to search for error fares from airlines.
+So why would i do this?
 
-### Markdown
+##Day 1: Choosing which language
+written on 05-03-2020
 
-Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
+####Goals
+I have 2 goals, to create a serious project in another programming paradigm. 
+And to find cheaper fare when an employee of an airline screws up. 
+With this i mean that the program detects when a flight is available for an abnormal price.
+For example: A flight from AMS to NYC normally costs around 600 euro's return, 
+but the application should detect when a flight suddenly lists for 40 euro's and give a warning.
+Because ill use it myself it should not need an advanced control panel. 
+An ini file should be fine to tell the application for which routes to search.
 
-```markdown
-Syntax highlighted code block
+###So... Why Haskell?
+As told before, i want to learn to program in a new programming paradigm. 
+After looking to diffrent paradigms I chose haskell over other languages because I found the following Pro's multiple times
 
-# Header 1
-## Header 2
-### Header 3
+* Clean, clear, readable code
+* New way of looking at problems
+* Decent performance; excellent concurrent performance for stuff like web back-ends (lightweight concurrency)
+* Easier to reason about what code is doing
+* Lots of libraries
 
-- Bulleted
-- List
+However there should be some cons as well:
+* Hard to learn; hence fewer developers
+    * Jobs?  What jobs?  (So far, this may well change)
+* Libraries are often ill-documented or overly-complicated
 
-1. Numbered
-2. List
+In this blog i will delve into the language itself and compare it to diffrent languages such as imperative (Java) against declarative.
 
-**Bold** and _Italic_ and `Code` text
+##Setting up the project
+Written on 06-03-2020
 
-[Link](url) and ![Image](src)
-```
+###Build tool, package manager and IDE
+I will use Stack as the build tool, this creates a local enviroment with the compiler, a default project structure and so on.
+As IDE i used Intellij with the Intellij-haskell plugin. However, 
+the goal of this blog is to point out diffrences in the programming language itself. NOt the package manager. Because of that i won't focus on those diffrences.
 
-For more details see [GitHub Flavored Markdown](https://guides.github.com/features/mastering-markdown/).
+##Start
+To start a project I made sure i had a decent project structure and think about the structure i want my project to get.
+I knew i want to have a component that imports my configuration, i knew i had to use some sort of headless browser or API to crawl available data, and i had to find a way to export it into a file.
 
-### Jekyll Themes
 
-Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/svenMolhuijsen/ErrorFareHaskellBlog/settings). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
 
-### Support or Contact
 
-Having trouble with Pages? Check out our [documentation](https://help.github.com/categories/github-pages-basics/) or [contact support](https://github.com/contact) and we’ll help you sort it out.
+
---
+++ out/production/Pong/Main.hs
@@ -0,0 +1,217 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Main where
+
+import           Graphics.Gloss
+import           Graphics.Gloss.Data.ViewPort       (ViewPort)
+import           Graphics.Gloss.Interface.Pure.Game
+
+width, height, offset :: Int
+width = 400
+
+height = 400
+
+offset = 200
+
+window :: Display
+window = InWindow "BlogPong" (width, height) (offset, offset)
+
+background :: Color
+background = yellow
+
+paddleWidth, paddleHeight, paddleDistance, ballRadius :: Float
+paddleWidth = 10
+
+paddleHeight = 100
+
+paddleDistance = 180
+
+ballRadius = 15
+
+-- | A data structure to hold the state of the Pong game.
+data PongGame =
+  Game
+    { ballLoc :: (Float, Float) -- ^ Pong ball (x, y) location.
+    , ballVel :: (Float, Float) -- ^ Pong ball (x, y) velocity.
+    , player1 :: Float -- ^ Left player paddle height.
+                                               -- Zero is the middle of the screen.
+    , player2 :: Float -- ^ Right player paddle height.
+    }
+  deriving (Show)
+
+-- | Convert a game state into a picture.
+render ::
+     PongGame -- ^ The game state to render.
+  -> Picture -- ^ A picture of this game state.
+render game =
+  pictures [ball, walls, mkPaddle black paddleDistance $ player1 game, mkPaddle black (-paddleDistance) $ player2 game]
+    --  The pong ball.
+  where
+    ballSize = ballRadius
+    ballColor = black
+    ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid ballSize
+    --  The bottom and top walls.
+    wall :: Float -> Picture
+    wall offset = translate 0 offset $ color wallColor $ rectangleSolid 400 50
+    wallColor = black
+    walls = pictures [wall 175, wall (-175)]
+    --  Make a paddle of a given border and vertical offset.
+    mkPaddle :: Color -> Float -> Float -> Picture
+    mkPaddle col x y =
+      pictures
+        [ translate (x) (y) $ color col $ rectangleSolid paddleWidth paddleHeight
+        , translate (x) (y) $ color col $ rectangleSolid paddleWidth paddleHeight
+        ]
+    paddleColor = light (light blue)
+
+-- | The starting state for the game of Pong.
+initialState :: PongGame
+initialState = Game {ballLoc = (-10, 30), ballVel = (-100, -30), player1 = 40, player2 = -80}
+
+-- | Respond to key events.
+handleKeys :: Event -> PongGame -> PongGame
+handleKeys (EventKey (Char 'r') _ _ _) game = game {ballLoc = (0, 0)}
+--handleKeys (EventKey (Char 'w') _ _ _)
+--
+--  game = game {player1 = ()}
+handleKeys (EventKey (SpecialKey KeyUp) _ _ _) game = game {player1 = y}
+  where
+    y1 = player1 game
+    y =
+      if y1 < (100)
+        then y1 + 10
+        else y1
+handleKeys (EventKey (SpecialKey KeyDown) _ _ _) game = game {player1 = y}
+  where
+    y1 = player1 game
+    y =
+      if y1 > (-100)
+        then y1 - 10
+        else y1
+handleKeys (EventKey (Char 'w') _ _ _) game = game {player2 = y}
+  where
+    y1 = player2 game
+    y =
+      if y1 < (100)
+        then y1 + 10
+        else y1
+handleKeys (EventKey (Char 's') _ _ _) game = game {player2 = y}
+  where
+    y1 = player2 game
+    y =
+      if y1 > (-100)
+        then y1 - 10
+        else y1
+-- Do nothing for all other events.
+handleKeys _ game = game
+
+moveBall ::
+     Float -- ^ The number of seconds since last update
+  -> PongGame -- ^ The initial game state
+  -> PongGame -- ^ A new game state with an updated ball position
+moveBall seconds game = game {ballLoc = (x', y')}
+    -- Old locations and velocities.
+  where
+    (x, y) = ballLoc game
+    (vx, vy) = ballVel game
+    -- New locations.
+    x' = x + vx * seconds
+    y' = y + vy * seconds
+
+-- | Detect a collision with a paddle. Upon collisions,
+-- change the velocity of the ball to bounce it off the paddle.
+paddleBounce :: PongGame -> PongGame
+paddleBounce game = game {ballVel = (vx', vy)}
+  where
+    radius = 10
+    (vx, vy) = ballVel game
+    vx' =
+      if paddleCollision (game)
+        then -vx
+        else vx
+
+-- | Given position and radius of the ball, return whether a collision occurred.
+paddleCollision ::
+     PongGame -- ^ The game
+  -> Bool -- ^ Collision with the paddles?
+paddleCollision game =
+  ((deltaXP1 * deltaXP1 + deltaYP1 * deltaYP1) < (ballRadius * ballRadius)) ||
+  ((deltaXP2 * deltaXP2 + deltaYP2 * deltaYP2) < (ballRadius * ballRadius))
+            -- Ball's center
+  where
+    (ballX, ballY) = ballLoc game
+            -- Player 1's paddle's center
+    recXP1 = paddleDistance
+    recYP1 = player1 game
+            -- Player 2's paddle's center
+    recXP2 = -paddleDistance
+    recYP2 = player2 game
+            -- Player A's paddle's left bottom corner (needed for collision math)
+    rectCornerXP1 = recXP1 - paddleWidth / 2
+    rectCornerYP1 = recYP1 - paddleHeight / 2
+            -- Player 2's paddle's left bottom corner (needed for collision math)
+    rectCornerXP2 = recXP2 - paddleWidth / 2
+    rectCornerYP2 = recYP2 - paddleHeight / 2
+    deltaXP1 = ballX - max rectCornerXP1 (min ballX (rectCornerXP1 + paddleWidth))
+    deltaYP1 = ballY - max rectCornerYP1 (min ballY (rectCornerYP1 + paddleHeight))
+    deltaXP2 = ballX - max rectCornerXP2 (min ballX (rectCornerXP2 + paddleWidth))
+    deltaYP2 = ballY - max rectCornerYP2 (min ballY (rectCornerYP2 + paddleHeight))
+    padLx2 = player2 game
+
+-- | Detect a collision with one of the side walls. Upon collisions,
+-- update the velocity of the ball to bounce it off the wall.
+wallBounce :: PongGame -> PongGame
+wallBounce game = game {ballVel = (vx, vy')}
+    -- Radius. Use the same thing as in `render`.
+    -- The old velocities.
+  where
+    (vx, vy) = ballVel game
+    vy' =
+      if wallCollision (ballLoc game) ballRadius
+             -- Update the velocity.
+        then -vy
+            -- Do nothing. Return the old velocity.
+        else vy
+
+type Radius = Float
+
+type Position = (Float, Float)
+
+-- | Given position and radius of the ball, return whether a collision occurred.
+wallCollision :: Position -> Radius -> Bool
+wallCollision (_, y) radius = topCollision || bottomCollision
+  where
+    topCollision = y - radius <= -fromIntegral width / 2 + 50
+    bottomCollision = y + radius >= fromIntegral width / 2 - 50
+
+-- | Detect a collision with one of the side walls. Upon collisions,
+-- update the velocity of the ball to bounce it off the wall.
+borderBounce :: PongGame -> PongGame
+borderBounce game = game {ballLoc = (vx', vy')}
+                             -- Radius. Use the same thing as in `render`.
+                             -- The old velocities.
+  where
+    (vx, vy) = ballLoc game
+    (vx', vy') =
+      if borderCollision (ballLoc game) ballRadius
+                                      -- Update the velocity.
+        then (0, 0)
+                                     -- Do nothing. Return the old velocity.
+        else (vx, vy)
+
+borderCollision :: Position -> Radius -> Bool
+borderCollision (x, _) radius = leftCollision || rightCollision
+  where
+    leftCollision = x - radius <= -fromIntegral width / 2
+    rightCollision = x + radius >= fromIntegral width / 2
+
+-- | Number of frames to show per second.
+fps :: Int
+fps = 60
+
+-- | Update the game by moving the ball and bouncing off walls.
+update :: Float -> PongGame -> PongGame
+update seconds = wallBounce . paddleBounce . moveBall seconds . borderBounce
+
+main :: IO ()
+main = play window background fps initialState render handleKeys update
